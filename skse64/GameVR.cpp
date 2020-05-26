13aae4a0ed5a25e2b1e9722ccc2ea97c2a735ad8
rollingrock <rollingrock16@gmail.com>
2020-05-26 11:13:03 -0500

    Removed all skse source.  cleaned up some stuff.   Integrated CommonLibVR.    This builds but needs some polish and testing

--- skse64/GameVR.cpp
+++
@@ -1,4 +0,0 @@
-#include "GameVR.h"
-
-RelocPtr <BSOpenVR *> g_openVR(0x02FEB9B0);
-RelocAddr<_HmdMatrixToNiTransform> HmdMatrixToNiTransform(0x00C57700);
--- skse64_loader_common/Inject.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "skse64_loader_common/IdentifyEXE.h"
-
-bool InjectDLL(PROCESS_INFORMATION * info, const char * dllPath, ProcHookInfo * hookInfo);
-bool InjectDLLThread(PROCESS_INFORMATION * info, const char * dllPath, bool sync, bool noTimeout);
